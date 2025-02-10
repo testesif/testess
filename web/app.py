754cdf54bfb2df86b8762ff703b77c8e731d90d4
testesif <isaacdra@gmail.com>
2025-02-10 09:14:55 -0300

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db:5432/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        new_user = User(name=name, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('form'))
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            Email: <input type="text" name="email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    '''
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
